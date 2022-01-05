bfe67646f1b970ba381e0b556ad6c103f8e55c8b
arifrhm <96586737+arifrhm@users.noreply.github.com>
2022-01-05 23:08:57 +0700

    Add files via upload

---
+++ Tugas5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"Inputkan Nilai Yang Ingin Dipangkatkan : ";
+	cin>>x;
+	int y;
+	cout<<"Inputkan Nilai Pangkat : ";
+	cin>>y;
+	cout<<"Hasil Per Pangkatan : "<<pow(x,y)<<endl;
+	return 0;
+}
